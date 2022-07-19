4dc6cadf50abfc559d8393b4fb2755229c44e05c
SJ.Lee <eagleduk@gmail.com>
2022-07-19 23:19:34 +0900

    project init
    패키지 설치..

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  if (true) {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontsPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+
+    return Promise.all([...fontsPromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
   }
   return (
     <View style={styles.container}>
